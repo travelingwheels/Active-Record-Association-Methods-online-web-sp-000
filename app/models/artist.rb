79d7da09d5ca82d2f7dffbf12950f928326ddb1b
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 10:02:20 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.songs.
+    self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.count(:songs)
   end
 
   def genre_count
